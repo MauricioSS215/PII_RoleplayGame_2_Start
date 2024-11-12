72903d738333e944c30a71e1f994a744203027e3
Ale Hernandez <alehernandez@gmail.com>
2024-11-12 00:33:21 -0300

    Plumber(enemy) añadido

---
+++ src/Library/Characters/Plumber.cs
@@ -0,0 +1,9 @@
+﻿namespace Ucu.Poo.RoleplayGame;
+
+public class Plumber : Enemies
+{
+    public Plumber(string nombre, List<Item> items, double vida, int vp) : base(nombre, items, vida, vp)
+    {
+        //Literalmente es un fontanero
+    }
+}
